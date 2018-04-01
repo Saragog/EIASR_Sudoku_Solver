bdd7808a4a1df1d248f4b745ba1aee28da898172
Saragog <andrzejdackiewicz@gmail.com>
2018-04-02 00:11:05 +0200

    Adding OpenCV basic application to the project

--- Sudoku_Solver/Sudoku_SolverDlg.cpp
+++ Sudoku_Solver/Sudoku_SolverDlg.cpp
@@ -6,6 +6,8 @@
 #include "Sudoku_Solver.h"
 #include "Sudoku_SolverDlg.h"
 #include "afxdialogex.h"
+#include<opencv2/opencv.hpp>
+#include<conio.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -64,6 +66,7 @@ BEGIN_MESSAGE_MAP(CSudoku_SolverDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CSudoku_SolverDlg::OnBnClickedOk)
 END_MESSAGE_MAP()
 
 
@@ -152,3 +155,43 @@ HCURSOR CSudoku_SolverDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CSudoku_SolverDlg::OnBnClickedOk()
+{
+	cv::Mat imgOriginal;        // input image
+	cv::Mat imgGrayscale;       // grayscale of input image
+	cv::Mat imgBlurred;         // intermediate blured image
+	cv::Mat imgCanny;           // Canny edge image
+
+
+	imgOriginal = cv::imread("image.png");          // open image
+
+	if (imgOriginal.empty()) {                                  // if unable to open image
+		std::cout << "error: image not read from file\n\n";     // show error message on command line
+		_getch();                                               // may have to modify this line if not using Windows
+		return;                                              // and exit program
+	}
+
+	cv::cvtColor(imgOriginal, imgGrayscale, CV_BGR2GRAY);       // convert to grayscale
+
+	cv::GaussianBlur(imgGrayscale,          // input image
+		imgBlurred,                         // output image
+		cv::Size(5, 5),                     // smoothing window width and height in pixels
+		1.5);                               // sigma value, determines how much the image will be blurred
+
+	cv::Canny(imgBlurred,           // input image
+		imgCanny,                   // output image
+		82,                         // low threshold
+		164);                       // high threshold
+
+									// declare windows
+	cv::namedWindow("imgOriginal", CV_WINDOW_AUTOSIZE);     // note: you can use CV_WINDOW_NORMAL which allows resizing the window
+	cv::namedWindow("imgCanny", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
+															// CV_WINDOW_AUTOSIZE is the default
+	cv::imshow("imgOriginal", imgOriginal);     // show windows
+	cv::imshow("imgCanny", imgCanny);
+
+	cv::waitKey(0);                 // hold windows open until user presses a key
+	return;
+}
