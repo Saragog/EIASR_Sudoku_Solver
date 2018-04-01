f277b32fdf32d0a055d3aec0ab9b20f9e63c2000
Mateusz Jarzemski <mat>
2018-04-01 21:52:01 +0200

    Pierwsze ustawienie projektu

---
+++ Sudoku_Solver/Sudoku_Solver.h
@@ -0,0 +1,32 @@
+
+// Sudoku_Solver.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CSudoku_SolverApp:
+// See Sudoku_Solver.cpp for the implementation of this class
+//
+
+class CSudoku_SolverApp : public CWinApp
+{
+public:
+	CSudoku_SolverApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSudoku_SolverApp theApp;
---
+++ Sudoku_Solver/Sudoku_SolverDlg.h
@@ -0,0 +1,34 @@
+
+// Sudoku_SolverDlg.h : header file
+//
+
+#pragma once
+
+
+// CSudoku_SolverDlg dialog
+class CSudoku_SolverDlg : public CDialogEx
+{
+// Construction
+public:
+	CSudoku_SolverDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SUDOKU_SOLVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
